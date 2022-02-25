d5a948195df8cc7a42514c79c0ad3e2a689cadf3
Schalk van Dyk <schalkvandyk1979@gmail.com>
2022-02-25 15:32:58 +0200

    create WelcomeBanner.cs and move class to file

--- HangMan0021/Program.cs
+++ HangMan0021/Program.cs
@@ -6,43 +6,12 @@ using System.Threading.Tasks;
 
 namespace HangMan0021
 {
-    public class WelcomeBanner
-    {
-        public static void DisplayWelcomeBanner()
-        {
-            StringBuilder stringBuilder = new StringBuilder();
-            stringBuilder.Append(' ', 20);
-            stringBuilder.Append('#', 31);
-            stringBuilder.Append('\n');
-            stringBuilder.Append(' ', 20);
-            stringBuilder.Append('#', 1);
-            stringBuilder.Append(' ', 29);
-            stringBuilder.Append('#', 1);
-            stringBuilder.Append('\n');
-            stringBuilder.Append(' ', 20);
-            stringBuilder.Append('#', 1);
-            stringBuilder.Append(' ', 5);
-            stringBuilder.Append("Welcome to Hangman!");
-            stringBuilder.Append(' ', 5);
-            stringBuilder.Append('#', 1);
-            stringBuilder.Append('\n');
-            stringBuilder.Append(' ', 20);
-            stringBuilder.Append('#', 1);
-            stringBuilder.Append(' ', 29);
-            stringBuilder.Append('#', 1);
-            stringBuilder.Append('\n');
-            stringBuilder.Append(' ', 20);
-            stringBuilder.Append('#', 31);
-
-
-            Console.WriteLine(stringBuilder);
-        }
-    }
     internal class Program
     {
         static void Main(string[] args)
         {
             WelcomeBanner.DisplayWelcomeBanner();
+
         }
     }
 }
---
+++ HangMan0021/WelcomeBanner.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+
+namespace HangMan0021
+{
+    public class WelcomeBanner
+    {
+        public static void DisplayWelcomeBanner()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(' ', 20);
+            stringBuilder.Append('#', 31);
+            stringBuilder.Append('\n');
+            stringBuilder.Append(' ', 20);
+            stringBuilder.Append('#', 1);
+            stringBuilder.Append(' ', 29);
+            stringBuilder.Append('#', 1);
+            stringBuilder.Append('\n');
+            stringBuilder.Append(' ', 20);
+            stringBuilder.Append('#', 1);
+            stringBuilder.Append(' ', 5);
+            stringBuilder.Append("Welcome to Hangman!");
+            stringBuilder.Append(' ', 5);
+            stringBuilder.Append('#', 1);
+            stringBuilder.Append('\n');
+            stringBuilder.Append(' ', 20);
+            stringBuilder.Append('#', 1);
+            stringBuilder.Append(' ', 29);
+            stringBuilder.Append('#', 1);
+            stringBuilder.Append('\n');
+            stringBuilder.Append(' ', 20);
+            stringBuilder.Append('#', 31);
+
+
+            Console.WriteLine(stringBuilder);
+        }
+    }
+}
