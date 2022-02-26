8e69e77e7408a03bc885edac58fd1bc3f55d638e
Schalk van Dyk <schalkvandyk1979@gmail.com>
2022-02-26 12:17:55 +0200

    add WillUserPlay.cs and code

---
+++ HangMan0021/WillUserPlay.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HangMan0021
+{
+    internal class WillUserPlay
+    {
+        public static bool UserReply()
+        {
+            Console.Write("\tDo you want to play? y/n: ");
+            var userInput = Console.ReadLine();
+            var willPlay = userInput == "y" ? true : false;
+            return willPlay; 
+        }
+    }
+}
