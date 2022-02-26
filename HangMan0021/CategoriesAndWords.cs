04efad9caa9d18fdc4151681c03775a06e073ec8
Schalk van Dyk <schalkvandyk1979@gmail.com>
2022-02-26 12:21:18 +0200

    add Category.cs and CategoriesAndWords.cs and code

---
+++ HangMan0021/CategoriesAndWords.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HangMan0021
+{
+    internal class CategoriesAndWords
+    {
+        public static List<string> Categories = new List<string>() { "fruits", "mamals", "sports"};
+        public static List<string> Fruits = new List<string>() { "orange", "watermelon", "strawberry", "grape", "kiwi"};
+        public static List<string> Mamals = new List<string>() { "sheep", "giraffe", "elephant", "kangaroo", "monkey"};
+        public static List<string> Sports = new List<string>() { "cricket", "rugby", "tennis", "batminton", "iceskating"};
+    }
+}
---
+++ HangMan0021/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HangMan0021
+{
+    internal class Category
+    {
+        public static string GetCategory()
+        {
+            Random randomNum = new Random();
+            var randomIndex = randomNum.Next(0, CategoriesAndWords.Categories.Count);
+
+            return CategoriesAndWords.Categories[randomIndex];
+        }
+    }
+}
